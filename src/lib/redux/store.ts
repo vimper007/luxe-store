80118b8498e34666c0d0c4b4851edfe14243ebc8
Vimukthi Perera <vimper007@gmail.com>
2025-11-30 18:31:32 +0530

    feat: Update Redux store configuration to include middleware with serializableCheck disabled

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -2,6 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
   reducer: {},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
